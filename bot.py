82e534da0ebe87f0eb78d78d1cd4a81448aaf0fa
oluiamerico <luiamericobusiness@gmail.com>
2026-04-03 15:53:13 -0300

    a

--- bot.py
+++ bot.py
@@ -11,17 +11,17 @@ user_steps = {}
 def start(message):
     chat_id = message.chat.id
     user_steps[chat_id] = 1
-    
+    time.sleep(5)
     bot.send_chat_action(chat_id, 'typing')
-    time.sleep(2)
+    time.sleep(5)
     bot.send_message(chat_id, "oi gatinho")
     
     bot.send_chat_action(chat_id, 'typing')
-    time.sleep(3)
+    time.sleep(6)
     bot.send_message(chat_id, "Me chamo Alessandra, tenho 19 aninhos... prazer 😉")
     
     bot.send_chat_action(chat_id, 'typing')
-    time.sleep(2)
+    time.sleep(4)
     bot.send_message(chat_id, "Posso falar dos meus conteúdos e das chamadas? 😈🔥")
 
 @bot.message_handler(func=lambda message: True)
@@ -32,6 +32,7 @@ def control_flow(message):
 
     # PASSO 1 -> PASSO 2 (Início do convite)
     if step == 1:
+        time.sleep(5)
         bot.send_chat_action(chat_id, 'upload_photo')
         try:
             with open('media/1.jpg', 'rb') as photo:
@@ -39,40 +40,41 @@ def control_flow(message):
         except: pass
 
         bot.send_chat_action(chat_id, 'record_voice')
-        time.sleep(4)
+        time.sleep(7)
         try:
             with open('media/1.ogg', 'rb') as voice:
                 bot.send_voice(chat_id, voice)
         except: pass
 
         bot.send_chat_action(chat_id, 'typing')
-        time.sleep(2)
+        time.sleep(5)
         bot.send_message(chat_id, "Quer ver minha bucetinha amor? 😚❤️")
         user_steps[chat_id] = 2
 
     # PASSO 2 -> PASSO 3 (Amostra 1)
     elif step == 2:
         bot.send_chat_action(chat_id, 'typing')
-        time.sleep(2)
+        time.sleep(5)
         bot.send_message(chat_id, "Vou te mandar gatinho, mas não pode mostrar para ninguem em.... 😈")
-        
+        time.sleep(10)
         bot.send_chat_action(chat_id, 'upload_video')
         try:
             with open('media/1.mp4', 'rb') as video:
                 bot.send_video(chat_id, video, caption="Pra você gatinho, espero que goste 🥰")
         except: pass
 
-        time.sleep(2)
+        time.sleep(4)
         bot.send_message(chat_id, "Quer ver mais?")
         user_steps[chat_id] = 3
 
     # PASSO 3 -> PASSO 4 (O Combo de Vídeos Amostra + Áudio + Oferta)
     elif step == 3:
         bot.send_chat_action(chat_id, 'typing')
-        time.sleep(1)
+        time.sleep(4)
         bot.send_message(chat_id, "🥰❤️❤️")
+        time.sleep(5)
         bot.send_message(chat_id, "Vou te mandar uma amostra do meu grupinho vip só pq gostei de você.... vou confiar 🙈")
-        
+        time.sleep(10)
         # Envio dos 3 vídeos de amostra em sequência
         for i in range(1, 4): # vídeos media/amostra_1.mp4, amostra_2.mp4, amostra_3.mp4
             bot.send_chat_action(chat_id, 'upload_video')
@@ -84,31 +86,32 @@ def control_flow(message):
 
         # Áudio "Vai me dar?"
         bot.send_chat_action(chat_id, 'record_voice')
-        time.sleep(3)
+        time.sleep(6)
         try:
             with open('media/2.ogg', 'rb') as v:
                 bot.send_voice(chat_id, v)
         except: pass
 
         bot.send_chat_action(chat_id, 'typing')
-        time.sleep(2)
+        time.sleep(4)
         bot.send_message(chat_id, "Vai me dar? 😏💦")
         user_steps[chat_id] = 4
 
     # PASSO 4 -> PASSO 5 (Print do Canal e Fechamento)
     elif step == 4:
         bot.send_chat_action(chat_id, 'typing')
-        time.sleep(2)
+        time.sleep(5)
         bot.send_message(chat_id, "Safadinho... ☺️❤️")
         
         bot.send_chat_action(chat_id, 'typing')
-        time.sleep(3)
+        time.sleep(8)
         bot.send_message(chat_id, "Se voce virar meu cliente eu faço uma punheta guiada gemendo gostoso... pra você gozar pensando na minha bucetinha rosa 😈")
         
         bot.send_chat_action(chat_id, 'typing')
-        time.sleep(2)
+        time.sleep(6)
         bot.send_message(chat_id, "Posso fazer tudo isso para você por R$19,90 anjo, o grupo VIP + Punhetinha Guiada de 30 minutos ❤️")
         
+        time.sleep(5)
         bot.send_message(chat_id, "Vai querer gatinho? 🔥")
         user_steps[chat_id] = 5
 
@@ -116,7 +119,7 @@ def control_flow(message):
     elif step == 5:
         # Áudio Final
         bot.send_chat_action(chat_id, 'record_voice')
-        time.sleep(4)
+        time.sleep(7)
         try:
             with open('media/3.ogg', 'rb') as v:
                 bot.send_voice(chat_id, v)
@@ -133,14 +136,16 @@ def control_flow(message):
         bot.send_message(chat_id, "Vou gerar minha chave pix pra você bb 😘🔥")
         
         bot.send_chat_action(chat_id, 'typing')
-        time.sleep(2)
+        time.sleep(7)
         bot.send_message(chat_id, "✅ Prontinho amor, gerei a chave pix!\n\n- Copie a Chave Pix \"copia e cola\" abaixo para realizar o pagamento ⤵️")
-        
+        time.sleep(6)
         # Sua chave PIX real (substituída pela da imagem)
         bot.send_message(chat_id, "00020101021226810014br.gov.bcb.pix2559qr.woovi.com/qr/v2/cob/6454384b-36d9-48e8-861b-36276c5ea40e520400005303986540519.905802BR5909PUSHINPAY6011HORTOLANDIA62290525f24d170d57724805ada1ae08c6304D647")
-        
+        time.sleep(5)
         bot.send_message(chat_id, "O acesso ao meu grupo vip com minhas fotos e videos são R$19,90 ta anjo?")
+        time.sleep(5)
         bot.send_message(chat_id, "vou te mandar o link do grupo assim que realizar o pagamento 🥰🥰❤️")
+        time.sleep(5)
         bot.send_message(chat_id, "Você só paga uma vez e tem acesso vitalício ❤️")
         user_steps[chat_id] = 0
 
